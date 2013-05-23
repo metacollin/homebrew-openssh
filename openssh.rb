ca22c7adf9d015791075c017c55c8ac2c629df9c
Collin Anderson <collinanderson@flamingpenguin.metacollin.net>
2013-05-22 19:00:18 -0600

    Pimp slap#

---
+++ openssh.rb
@@ -0,0 +1,60 @@
+require 'formula'
+
+class Openssh < Formula
+  homepage 'http://www.openssh.com/'
+  url 'http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.2p1.tar.gz'
+  version '6.2p1'
+  sha1 '8824708c617cc781b2bb29fa20bd905fd3d2a43d'
+
+  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+
+  depends_on 'openssl' if build.with? 'brewed-openssl'
+  depends_on 'ldns' => :optional
+  depends_on 'pkg-config' => :build if build.with? "ldns"
+
+  def patches
+    p = []
+    p << 'https://gist.github.com/metacollin/5559308/raw/96adc2d51c722a799de95e2e9f391bba24bcf371/openssh-6.2p1.patch' if build.include? 'with-keychain-support'
+    p
+  end
+
+
+  def install
+    if build.include? "with-keychain-support"
+        ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
+        ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
+    end
+
+    args = %W[
+      --with-libedit
+      --with-kerberos5
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}/ssh
+    ]
+
+    args << "--with-ssl-dir=#{Formula.factory('openssl').opt_prefix}" if build.with? 'brewed-openssl'
+    args << "--with-ldns" if build.with? "ldns"
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+  end
+
+  def caveats
+    if build.include? "with-keychain-support"
+      <<-EOS.undent
+        For complete functionality, please modify:
+          /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
+
+        and change ProgramArugments from
+          /usr/bin/ssh-agent
+        to
+          /usr/local/bin/ssh-agent
+
+        After that, you can start storing private key passwords in
+        your OS X Keychain.
+      EOS
+    end
+  end
+end
+
